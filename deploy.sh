57996a0169a5a99bf1913ced1e8dc639e4729060
David Martínez Cebada <davidcbd17@gmail.com>
2025-05-18 00:25:39 -0600

    Modificación en templates

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
+# En este script se despliega la aplicación en Google App Engine
+# Se debe tener instalado el SDK de Google Cloud y autenticarse con gcloud auth login
+
 gcloud app deploy --project pik-api
